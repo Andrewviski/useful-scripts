f3e32741694bc091ffca49ec4313b2bcb30b5a29
Andre Kaba <andree_kaba93@hotmail.com>
2016-09-13 23:10:39 -0400

    Add installPycharmpro bash script

---
+++ installPyCharmPro.sh
@@ -0,0 +1,6 @@
+wget https://download-cf.jetbrains.com/python/pycharm-professional-2016.2.3.tar.gz
+mv pycharm-professional-2016.2.3.tar.gz ~
+tar -xvzf pycharm-professional-2016.2.3.tar.gz
+rm pycharm-professional-2016.2.3.tar.gz
+mv pycharm-professional-2016.2.3/ pycharm-pro/
+
