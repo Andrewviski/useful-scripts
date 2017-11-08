839b4db0a4d0d5c59799f730f9f153c73ce8cbe2
Andre Kaba <andree_kaba93@hotmail.com>
2017-11-07 19:28:50 -0500

    remove repeated code in bashrc

--- add_to_bashrc.sh
+++ add_to_bashrc.sh
@@ -14,20 +14,5 @@ function mcgill () {
 function sys_update(){
 	sudo apt update && sudo apt dist-upgrade -
 }
-export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[33;1m\]\w\[\033[m\] =>"
-export CLICOLOR=1
-export LSCOLORS=ExFxBxDxCxegedabagacad
-
-function cs () {
-    cd "$@" && ls
-}
-
-function mcgill () {
-    cd $HOME'/Dropbox/McGill 2017-2018'
-}
-
-function sys_update(){
-    sudo apt update && sudo apt dist-upgrade -y
-}
 
 
